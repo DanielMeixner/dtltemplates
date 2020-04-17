c5bdb4a0c38e670f0c1d217d40aa9e561ecc7207
Daniel Meixner <daniel.meixner@live.com>
2020-04-17 13:54:44 +0200

    uncomment installer

--- artifacts/vso/install.ps1
+++ artifacts/vso/install.ps1
@@ -14,27 +14,27 @@ param(
 )
 
 
-# Set-StrictMode -Version Latest
-# $ErrorActionPreference = "Stop"
-# $PSDefaultParameterValues['*:ErrorAction'] = 'Stop'
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+$PSDefaultParameterValues['*:ErrorAction'] = 'Stop'
 
-# Import-Module -Name "Microsoft.PowerShell.Archive"
-# $source = "https://vsoagentdownloads.blob.core.windows.net/vsoagent/VSOAgent_win_3633968.zip"
-# $webClient = New-Object System.Net.WebClient
-# $firstLetters = (New-Guid).ToString().SubString(0, 4)
-# $tempdestination = Join-Path -Path $env:TEMP  -ChildPath ("\vsoDownload" + $firstLetters +".zip")
+Import-Module -Name "Microsoft.PowerShell.Archive"
+$source = "https://vsoagentdownloads.blob.core.windows.net/vsoagent/VSOAgent_win_3633968.zip"
+$webClient = New-Object System.Net.WebClient
+$firstLetters = (New-Guid).ToString().SubString(0, 4)
+$tempdestination = Join-Path -Path $env:TEMP  -ChildPath ("\vsoDownload" + $firstLetters +".zip")
 
 
-# $WebClient.DownloadFile($source, $tempdestination)
+$WebClient.DownloadFile($source, $tempdestination)
 
-# $destination=Join-Path -Path $env:SystemDrive -ChildPath "VSOnline"
-# Expand-Archive -Path $tempdestination -Destination $destination -Force
-# Write-Host "Installed VSO to:" $destination
-# Write-Host "Run vso start to create your environment!"
-# $runpath=$destination+"\vso.exe"
+$destination=Join-Path -Path $env:SystemDrive -ChildPath "VSOnline"
+Expand-Archive -Path $tempdestination -Destination $destination -Force
+Write-Host "Installed VSO to:" $destination
+Write-Host "Run vso start to create your environment!"
+$runpath=$destination+"\vso.exe"
 
-# & $runpath  start -r vso-rg-f0574e4 -s 7f28d486-9ef2-4bd9-a295-e66f5949c6b2 -n vso-plan-westeurope
-# Write-Host "VSO started"
+& $runpath  start -r vso-rg-f0574e4 -s 7f28d486-9ef2-4bd9-a295-e66f5949c6b2 -n vso-plan-westeurope
+Write-Host "VSO started"
 $authrelayurl = "https://prod-95.westeurope.logic.azure.com:443/workflows/23f06675f48646998a91d97a55a56235/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=50KJExnmWKyTiN5WwT1X8o6ekd7KAOZwqboO-VWdZig"
 $guid = New-Guid
 Write-Host "GUID" + $guid
@@ -42,7 +42,7 @@ $outfilename=".\out"+$guid+".txt"
 Write-Host $outfilename
 
 $args=" start -r "+ $resourcegroup+" -s "+$subscriptionid+"-n "+ $planname
-# $args=" start -r vso-rg-f0574e4 -s 7f28d486-9ef2-4bd9-a295-e66f5949c6b2 -n vso-plan-westeurope"
+
 Write-Host "argslist: " + $args
 
 Start-Process C:\VSOnline\vso -ArgumentList $args -RedirectStandardOutput $outfilename -WindowStyle Hidden
