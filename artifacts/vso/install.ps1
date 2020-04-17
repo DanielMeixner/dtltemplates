5879f3f8be279ea18b4e32dd14c639a432550ec3
Daniel Meixner <daniel.meixner@live.com>
2020-04-17 14:45:34 +0200

    -UseBasicParsing

--- artifacts/vso/install.ps1
+++ artifacts/vso/install.ps1
@@ -22,42 +22,42 @@ Import-Module -Name "Microsoft.PowerShell.Archive"
 $source = "https://vsoagentdownloads.blob.core.windows.net/vsoagent/VSOAgent_win_3633968.zip"
 $webClient = New-Object System.Net.WebClient
 $firstLetters = (New-Guid).ToString().SubString(0, 4)
-$tempdestination = Join-Path -Path $env:TEMP  -ChildPath ("\vsoDownload" + $firstLetters +".zip")
+$tempdestination = Join-Path -Path $env:TEMP  -ChildPath ("\vsoDownload" + $firstLetters + ".zip")
 
 
 $WebClient.DownloadFile($source, $tempdestination)
 
-$destination=Join-Path -Path $env:SystemDrive -ChildPath "VSOnline"
+$destination = Join-Path -Path $env:SystemDrive -ChildPath "VSOnline"
 Expand-Archive -Path $tempdestination -Destination $destination -Force
 Write-Host "Installed VSO to:" $destination
 Write-Host "Run vso start to create your environment!"
-$runpath=$destination+"\vso.exe"
+$runpath = $destination + "\vso.exe"
 
 
 $authrelayurl = "https://prod-95.westeurope.logic.azure.com:443/workflows/23f06675f48646998a91d97a55a56235/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=50KJExnmWKyTiN5WwT1X8o6ekd7KAOZwqboO-VWdZig"
 $guid = New-Guid
 Write-Host "GUID" + $guid
-$outfilename=".\out"+$guid+".txt"
+$outfilename = ".\out" + $guid + ".txt"
 Write-Host $outfilename
 
-$args=" start -r "+ $resourcegroup+" -s "+$subscriptionid+" -n "+ $planname
+$args = " start -r " + $resourcegroup + " -s " + $subscriptionid + " -n " + $planname
 
 Write-Host "argslist: " + $args
 
 Start-Process $runpath -ArgumentList $args -RedirectStandardOutput $outfilename -WindowStyle Hidden
  
- # dirty for now - later wait for file being created and filled.
- Start-Sleep 5
+# dirty for now - later wait for file being created and filled.
+Start-Sleep 5
 
- $res= Get-Content $outfilename
+$res = Get-Content $outfilename
  
- Write-Host "res:"+$res
+Write-Host "res:"+$res
     
- $params = @{    
-    mail = 'daniel.meixner@microsoft.com'
-    message = $res +''
+$params = @{    
+    mail    = 'daniel.meixner@microsoft.com'
+    message = $res + ''
 }
- Invoke-WebRequest -Uri $authrelayurl -Method Post -Body ($params|ConvertTo-Json) -ContentType "application/json"
+Invoke-WebRequest -Uri $authrelayurl -Method Post -Body ($params | ConvertTo-Json) -ContentType "application/json" -UseBasicParsing
 Write-Host "sended"
 
  
