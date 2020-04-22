11dd8b4d191fbc2f6a23942869ae0d336c4e5285
Daniel Meixner <daniel.meixner@live.com>
2020-04-22 15:05:50 +0200

    added authurl

--- artifacts/vso-remove/uninstall.ps1
+++ artifacts/vso-remove/uninstall.ps1
@@ -32,6 +32,9 @@ $params = @{
     mail    = $mail + ''
     message = $res + ''
 }
+
+$authrelayurl = "https://prod-95.westeurope.logic.azure.com:443/workflows/23f06675f48646998a91d97a55a56235/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=50KJExnmWKyTiN5WwT1X8o6ekd7KAOZwqboO-VWdZig"
+
 Invoke-WebRequest -Uri $authrelayurl -Method Post -Body ($params | ConvertTo-Json) -ContentType "application/json" -UseBasicParsing
 Write-Host "sended"
 
